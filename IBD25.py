8f8bd13bfec6a9e8f94ba9591a7c2e0f7da0f1a4
Luke Tchang <luketchang@Lukes-MacBook-Pro.local>
2019-07-15 10:49:03 -0700

    first commit includes flat base conditions, IBD 25 hitting 52 week highs, IBD 25, 52 week highs, a Intrinio test document, and the old Excel parser document

---
+++ IBD25.py
@@ -0,0 +1,22 @@
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+from urllib.request import Request, urlopen
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = "https://ycharts.com/companies/FFTY/holdings"
+req = Request(url, headers={'User-Agent': 'Safari/8536.25'})
+html = urlopen(req, context=ctx).read()
+# html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+tags =  soup('a')
+for tag in tags:
+    possibleSymbol = tag.get('href', None)
+    if possibleSymbol is None:
+        continue
+    elif possibleSymbol.find('/companies/') >=0 and tag.string.find(possibleSymbol[11:len(possibleSymbol)]) != -1:
+        print(possibleSymbol[11:len(possibleSymbol)])
